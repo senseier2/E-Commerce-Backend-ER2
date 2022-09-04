2ad19de8bfb1ee838e8c72d152e86bb6808ecae4
Senfire <eric.mcghee@northwestern.edu>
2022-09-04 18:32:44 -0500

    Adding Models initial commit

---
+++ models/ProductTag.js
@@ -0,0 +1,37 @@
+const { Model, DataTypes } = require('sequelize');
+
+const sequelize = require('../config/connection');
+
+class ProductTag extends Model {}
+
+ProductTag.init(
+  {
+    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_id: {
+      type: DataTypes.INTEGER,
+      references:{
+      },
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        
+      },
+    }
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'product_tag',
+  }
+);
+
+module.exports = ProductTag;
